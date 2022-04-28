6399d16d990f232ac87b78d01c1a1ac55f958e36
Weronika_studia <w.motkowska#student.uw.edu.pl>
2022-04-28 23:12:58 +0200

    finished selenium fila and dataset

--- selenium/sel.py
+++ selenium/sel.py
@@ -63,7 +63,7 @@ def find_links1(property_links, length):
 def find_links2(i, property_links, length):
     url = 'https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/warszawa/?page=' + str(i)
     driver.get(url)
-    time.sleep(5)
+    time.sleep(2)
     property_links = find_links1(property_links, length)
     return property_links
 
@@ -102,17 +102,14 @@ d = pd.DataFrame({'name':[], 'price':[], 'price_m2':[], 'm2':[], 'rooms':[], 'fl
 # Scraping every link for information by accessing tags and cleaning the obtainedinformation
 for url in property_links:
     driver.get(url)
-    time.sleep(5)
+    time.sleep(15)
 
     print('* olx site scraping is at work *')
     name = driver.find_element(By.XPATH, '//h1').get_attribute("innerHTML").splitlines()[0]
     price = driver.find_element(By.XPATH, '//h3').get_attribute("innerHTML").splitlines()[0]
     price = price.split('zł', 1)[0]
     price = re.sub(" ", "", price)
-    price_m2 = driver.find_element(By.XPATH, '//html/body/div[1]/div[1]/div[3]/div[3]/div[1]/div[2]/ul/li[2]/p').get_attribute("innerHTML").splitlines()[0]
-    price_m2 = price_m2.split(' ', 4)[3]
-    map = ''
-    # map = 'Warszawa, ' + driver.find_element(By.XPATH, '//html/body/div[1]/div[1]/div[3]/div[3]/div[2]/div[2]/div/section/div[1]/div/p[1]/span').text
+    map = 'Warszawa, ' + driver.find_element(By.XPATH, '//html/body/div[1]/div[1]/div[3]/div[3]/div[2]/div[2]/div/section/div[1]/div/p[1]/span').text
     details = driver.find_elements(By.XPATH, '//p[@class="css-xl6fe0-Text eu5v0x0"]')
 
     for element in details:
@@ -124,52 +121,11 @@ for url in property_links:
             floor = element.split(' ', 1)[1]
         if "Liczba pokoi" in element:
             rooms = element.split(' ', 3)[2]
+        if "Cena za" in element:
+            price_m2 = element.split(' ', 4)[3]
 
     property = {'name':name, 'price':price, 'price_m2':price_m2, 'm2':m2, 'rooms':rooms, 'floor':floor, 'map':map, 'url':url}
     d = d.append(property, ignore_index = True)
 
 # Saving results to csv
 d.to_csv('data/data_selenium.csv')
-
-
-
-# UNUSED CODE FOR OTODOM
-
-# Clicking cookies consent
-# url = 'https://www.otodom.pl/'
-# driver.get(url)
-# time.sleep(1)
-# button = driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
-# button.click()
-
-# if regexp.search(url):
-#     print('* otodom scraping is at work *')
-#     name = driver.find_element(By.XPATH, '//h1').get_attribute("innerHTML").splitlines()[0]
-#     price = driver.find_element(By.XPATH, '//header/strong').get_attribute("innerHTML").splitlines()[0]
-#     price = price.split('zł', 1)[0]
-#     price = re.sub(" ", "", price)
-#     price_m2 = driver.find_element(By.XPATH, '//div[@aria-label="Cena za metr kwadratowy"]').get_attribute("innerHTML").splitlines()[0]
-#     price_m2 = price_m2.split('zł', 1)[0]
-#     price_m2 = re.sub(" ", "", price_m2)
-#     # m2 = driver.find_element(By.XPATH, '//div[@aria-label="Powierzchnia"/div[2]]/div').get_attribute("innerHTML").splitlines()[0]
-#     # rooms = driver.find_element(By.XPATH, '//div[@aria-label="Liczba pokoi"/div[2]]/div').get_attribute("innerHTML").splitlines()[0]
-#     # floor = driver.find_element(By.XPATH, '//div[@aria-label="Piętro"/div[2]]/div').get_attribute("innerHTML").splitlines()[0]
-#     # rent = driver.find_element(By.XPATH, '//div[@aria-label="Czynsz"/div[2]]/div').get_attribute("innerHTML").splitlines()[0]
-#     # description = driver.find_element(By.XPATH, '//main/div[3]/div[2]/section[2]/div/div/p').get_attribute("innerHTML").splitlines()[0]
-#     map = driver.find_element(By.XPATH, '//a[@aria-label="Adres"]').get_attribute("innerHTML").splitlines()[0]
-#     map = map.split('>', 4)[4]
-
-    # key_words = ''
-    # keywords = driver.find_elements(By.XPATH, '/html/body/div[1]/div[1]/div[3]/div[3]/div[1]/div[2]/ul/li/p')
-    # regexp3 = re.compile(r'span')
-    # for key in keywords:
-    #     if regexp3.search(key.get_attribute("innerHTML").splitlines()[0]):
-    #         key_words = key_words + driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[3]/div[3]/div[1]/div[2]/ul/li[1]/p/span').get_attribute("innerHTML").splitlines()[0] + ' '
-    #     else:
-    #         key_words = key_words + key.get_attribute("innerHTML").splitlines()[0] + ' '
-        
-    # try:
-    #     description = driver.find_element(By.XPATH, '//div[@class="css-g5mtbi-Text"]').get_attribute("innerHTML").splitlines()[0]
-    # except:
-    #     description = driver.find_element(By.XPATH, '//div[@class="css-g5mtbi-Text"]').get_attribute("innerHTML").splitlines()[0]
-
